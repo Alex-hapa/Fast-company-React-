5ce55e12fddeacdb848c5a551da9dc4398b6f330
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-11-26 15:30:38 +0200

    feat: add propTypes in users component

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
+import PropTypes from "prop-types";
 
 const Users = ({ users, ...rest }) => {
     const count = users.length;
@@ -43,5 +44,6 @@ const Users = ({ users, ...rest }) => {
         </>
     );
 };
+Users.propTypes = { users: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default Users;
