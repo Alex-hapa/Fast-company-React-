3ccc8093b58c2f2e730f5ee12796119ec7c60ec0
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-11-26 14:55:29 +0200

    feat: add propTypes in searchStatus component

--- src/app/components/searchStatus.jsx
+++ src/app/components/searchStatus.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const SearchStatus = ({ length }) => {
     const renderPhrase = (number) => {
@@ -21,5 +22,6 @@ const SearchStatus = ({ length }) => {
         </h2>
     );
 };
+SearchStatus.propTypes = { length: PropTypes.number.isRequired };
 
 export default SearchStatus;
