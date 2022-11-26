2e66cefc2e4ecba5df05c287826627c32d264c27
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-11-26 14:41:44 +0200

    feat: add propTypes in bookmark conponent

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const BookMark = ({ status, ...rest }) => {
     return (
@@ -7,5 +8,6 @@ const BookMark = ({ status, ...rest }) => {
         </button>
     );
 };
+BookMark.propTypes = { status: PropTypes.node.isRequired };
 
 export default BookMark;
